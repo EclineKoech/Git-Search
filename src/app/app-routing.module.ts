03281cbb4fd5574acbcc8afbefab6846b46ef50c
Ecline Koech <eclinejerutokoech@gmail.com>
2021-10-22 06:42:27 +0300

    add paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'search-form', component: SearchFormComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  // { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
